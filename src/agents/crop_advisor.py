409b207225e82e21bcf0ab9ab3c2f46fb601e278
Nihilantropy <crea@student.42roma.it>
2025-01-24 18:41:02 +0100

    resetting to default project

--- src/agents/crop_advisor.py
+++
@@ -1,5 +0,0 @@
-from llama_index.core.agent import ReActAgent
-
-class CropAgent:
-    def __init__(self):
-        self.agent = ReActAgent.from_tools(...)
--- src/main.py
+++
@@ -1,6 +0,0 @@
-from agents.crop_advisor import CropAgent
-from workflows.basic_qa import run_workflow
-
-if __name__ == "__main__":
-    agent = CropAgent()
-    run_workflow(agent)
